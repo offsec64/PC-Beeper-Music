6e12c50ebb3de42050af3963f71b40ebda7f4892
offsec64 <52871476+offsec64@users.noreply.github.com>
2019-12-12 18:13:46 -0700

    Add play all option

--- music.sh
+++ music.sh
@@ -12,11 +12,12 @@
 # Songs that play on your PC beep speaker.
 
 # Prompts menu using whiptail on which song to play, then plays it using the beep command at specific durations/frequencies.
-ADVSEL=$(whiptail --title "LINUXGAMES v1.0" --menu "Choose an option" 15 60 4 \
+ADVSEL=$(whiptail --title "PC Beeper Music v1.0" --menu "Choose an option" 15 60 4 \
         "1" "Mary Had a Little Lamb" \
         "2" "Ode to Joy" \
         "3" "Windows XP Startup" \
-        "4" "Twinkle Twinkle Little Star" 3>&1 1>&2 2>&3)
+        "4" "Twinkle Twinkle Little Star" \
+        "5" "All of them!" 3>&1 1>&2 2>&3)
     case $ADVSEL in
         1)
             echo ~~MARY HAD A LITTLE LAMB~~
@@ -118,5 +119,110 @@ ADVSEL=$(whiptail --title "LINUXGAMES v1.0" --menu "Choose an option" 15 60 4 \
             sleep .03
             beep -f 523 -l 800
         ;;
+        5)
+            echo ~~MARY HAD A LITTLE LAMB~~
+            beep -f 659
+            beep -f 587 
+            beep -f 523
+            beep -f 587
+            beep -f 659
+            beep -f 659
+            beep -f 659
+            beep -f 587
+            beep -f 587
+            beep -f 587
+            beep -f 659
+            beep -f 783
+            beep -f 783
+            beep -f 659
+            beep -f 587 
+            beep -f 523
+            beep -f 587
+            beep -f 659
+            beep -f 659
+            beep -f 659
+            beep -f 587 
+            beep -f 587
+            beep -f 587
+            beep -f 659
+            beep -f 659
+            beep -f 523 -l 900
+
+            sleep 1.5
+
+            echo ~~ODE TO JOY~~
+            beep -f 987 -l 400
+            beep -f 987 -l 400
+            beep -f 1046 -l 400
+            beep -f 1174 -l 400
+            beep -f 1174 -l 400
+            beep -f 1046 -l 400
+            beep -f 987 -l 400
+            beep -f 880 -l 400
+            beep -f 783 -l 400
+            beep -f 783 -l 400
+            beep -f 880 -l 400
+            beep -f 987 -l 400
+            beep -f 987 -l 600
+            beep -f 880 -l 200
+            beep -f 880 -l 800
+            beep -f 987 -l 400
+            beep -f 987 -l 400
+            beep -f 1046 -l 400
+            beep -f 1174 -l 400
+            beep -f 1174 -l 400
+            beep -f 1046 -l 400
+            beep -f 987 -l 400
+            beep -f 880 -l 400
+            beep -f 783 -l 400
+            beep -f 783 -l 400
+            beep -f 880 -l 400
+            beep -f 987 -l 400
+            beep -f 880 -l 600
+            beep -f 783 -l 200
+            beep -f 783 -l 1000
+
+            sleep 1.5
+
+            echo ~~WINDOWS XP STARTUP~~
+            beep -f 1245 -l 600
+            beep -f 622 -l 400
+            beep -f 932 -l 400
+            beep -f 830 -l 600
+            beep -f 1245 -l 400
+            beep -f 932 -l 1000
+
+            sleep 1.5
+
+            echo ~~TWINKLE TWINKLE LITTLE STAR~~
+            beep -f 523 -l 375
+            sleep .03
+            beep -f 523 -l 375
+            sleep .03
+            beep -f 783 -l 375
+            sleep .03
+            beep -f 783 -l 375
+            sleep .03
+            beep -f 880 -l 375
+            sleep .03
+            beep -f 880 -l 375
+            sleep .03
+            beep -f 783 -l 800
+            sleep .03
+            beep -f 698 -l 375
+            sleep .03
+            beep -f 698 -l 375
+            sleep .03
+            beep -f 659 -l 375
+            sleep .03
+            beep -f 659 -l 375
+            sleep .03
+            beep -f 587 -l 375
+            sleep .03
+            beep -f 587 -l 375
+            sleep .03
+            beep -f 523 -l 800
+        ;;
         
     esac
+
